26b26b427329a844205ddcd6b749180c56e06bc2
tanchik25 <karlovatatyana2503@gmail.com>
2020-08-20 18:26:08 +0300

    Updates

---
+++ precache-manifest.4d7204a41d811eb6ee1af473e116c9f6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d6635ac4070ccb6b350c06f0fdc6da",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "523af019e5646d110391",
+    "url": "/react-deploy/static/css/4.2768c405.chunk.css"
+  },
+  {
+    "revision": "77fa71c87d947b670c41",
+    "url": "/react-deploy/static/css/5.6869b78d.chunk.css"
+  },
+  {
+    "revision": "809720b72235979ba45b",
+    "url": "/react-deploy/static/css/main.21c6d744.chunk.css"
+  },
+  {
+    "revision": "39ed755e37cd92d43aaa",
+    "url": "/react-deploy/static/js/2.ae3da7ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-deploy/static/js/2.ae3da7ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12bb265d9e4a531f9069",
+    "url": "/react-deploy/static/js/3.057770fd.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/react-deploy/static/js/3.057770fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "523af019e5646d110391",
+    "url": "/react-deploy/static/js/4.d067c54a.chunk.js"
+  },
+  {
+    "revision": "77fa71c87d947b670c41",
+    "url": "/react-deploy/static/js/5.f44d956f.chunk.js"
+  },
+  {
+    "revision": "809720b72235979ba45b",
+    "url": "/react-deploy/static/js/main.0653b5dc.chunk.js"
+  },
+  {
+    "revision": "43d4f5ece0f37a93754a",
+    "url": "/react-deploy/static/js/runtime-main.9354bb52.js"
+  },
+  {
+    "revision": "6048125567f5db4f975d8083c95fb506",
+    "url": "/react-deploy/static/media/6ffaa98a51d44357ad91edb25fc8349c.60481255.png"
+  },
+  {
+    "revision": "462a0b54123bea84c00e86f3d734aad3",
+    "url": "/react-deploy/static/media/avatarka.462a0b54.jpg"
+  },
+  {
+    "revision": "8cf4a15f5339f83f3756ff6094fd89b8",
+    "url": "/react-deploy/static/media/avatarka2.8cf4a15f.jpg"
+  },
+  {
+    "revision": "1ba2d1858b30a2f17f1dc8e39811c2ec",
+    "url": "/react-deploy/static/media/myavatar.1ba2d185.jpg"
+  }
+]);
